46d434fe5ce39be703bbb692e20d24b177a4ba7e
Nick <nick.jugganaikloo@wavetronix.com>
2020-11-10 17:08:22 -0700

    created a simple example of the bug

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+namespace dotnetBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Will Call GetActiveUdpListeners()");
+            try{
+            var listeners =IPGlobalProperties.GetIPGlobalProperties().GetActiveUdpListeners();
+            }catch (Exception e){
+                Console.WriteLine("Failed while calling GetActiveUdpListeners()");
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}
